4017ea44605dc170c57cf31b6cbac8f1d0fdfec7
SuhyeongCho <41462927+SuhyeongCho@users.noreply.github.com>
2019-05-27 23:45:45 +0900

    소켓 예제

---
+++ 05주차/src/LocalLookup.java
@@ -0,0 +1,18 @@
+import java.net.*;
+ 
+public class LocalLookup {
+	public static void  main(String[] args){
+		
+		InetAddress inetaddr = null;
+		try {
+			inetaddr = InetAddress.getLocalHost();
+		}catch(UnknownHostException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Hostname : "+inetaddr.getHostName());
+		System.out.println("Fully qualified domainname : "+inetaddr.getCanonicalHostName());
+		
+		System.out.println("IP address : "+inetaddr.getHostAddress());
+		System.out.println("Hash value of ip : "+inetaddr.hashCode());
+	}
+}
